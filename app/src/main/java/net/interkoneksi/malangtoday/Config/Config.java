20780d73ecd5982f6adbb6044aaa73152e28906b
ardhanmz <ardhanandroid@gmail.com>
2017-07-29 11:52:10 +0700

    initial Build : - add splashscreen - load image to action bar

---
+++ app/src/main/java/net/interkoneksi/malangtoday/Config/Config.java
@@ -0,0 +1,8 @@
+package net.interkoneksi.malangtoday.Config;
+
+public class Config {
+        public static final String BASE_URL = "http://malangtoday.net/api/";
+        public static final String ALL_CATEGORY_URL = BASE_URL + "get_category/";
+        public static String GET_POST = "terbaru/";
+        public static String GET_ALL_POST= "list_terbaru";
+}
